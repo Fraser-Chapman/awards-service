fd1e62c50b4d21ee6b209165e1a41f884d59c41d
fraser.chapman <fraser.chapman@autotrader.co.uk>
2020-05-15 13:27:48 +0100

    add test for multiple winners of a category scenario

--- src/test/java/uk/co/tissueinc/awardsservice/service/AwardsServiceTest.java
+++ src/test/java/uk/co/tissueinc/awardsservice/service/AwardsServiceTest.java
@@ -17,6 +17,11 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class AwardsServiceTest {
 
+    private static final String CATEGORY_ONE = "categoryOne";
+    private static final String PERSON_1 = "person1";
+    private static final String PERSON_2 = "person2";
+    private static final String USER_1 = "user1";
+    private static final String USER_2 = "user2";
     private AwardsService testSubject;
 
     @Mock
@@ -29,9 +34,9 @@ class AwardsServiceTest {
 
     @Test
     public void getAwardsShouldCallNominationServiceForNominations() {
-        final Map<String, String> nominations = Map.of("categoryOne", "person1");
-        NominationsForm nominations1 = new NominationsForm("user1", nominations);
-        NominationsForm nominations2 = new NominationsForm("user2", nominations);
+        final Map<String, String> nominations = Map.of(CATEGORY_ONE, PERSON_1);
+        NominationsForm nominations1 = new NominationsForm(USER_1, nominations);
+        NominationsForm nominations2 = new NominationsForm(USER_2, nominations);
 
         when(nominationsService.getNominations()).thenReturn(List.of(nominations1, nominations2));
 
@@ -42,17 +47,28 @@ class AwardsServiceTest {
 
     @Test
     public void getAwardsShouldCalculateWinners() {
-        final Map<String, String> nominations = Map.of("categoryOne", "person1");
-        NominationsForm nominations1 = new NominationsForm("user1", nominations);
-        NominationsForm nominations2 = new NominationsForm("user2", nominations);
-        NominationsForm nominations3 = new NominationsForm("user3", Map.of("categoryOne", "person2"));
+        final Map<String, String> nominations = Map.of(CATEGORY_ONE, PERSON_1);
+        NominationsForm nominations1 = new NominationsForm(USER_1, nominations);
+        NominationsForm nominations2 = new NominationsForm(USER_2, nominations);
+        NominationsForm nominations3 = new NominationsForm("user3", Map.of(CATEGORY_ONE, PERSON_2));
 
         when(nominationsService.getNominations()).thenReturn(List.of(nominations1, nominations2, nominations3));
 
         List<CategoryAward> result = testSubject.getAwards();
 
         //TODO maybe sort the ordering!!!!!!
-        final List<CategoryAward> expectedResult = List.of(new CategoryAward("categoryOne", "person1", List.of("person2", "person1")));
+        final List<CategoryAward> expectedResult = List.of(new CategoryAward(CATEGORY_ONE, PERSON_1, List.of(PERSON_2, PERSON_1)));
+        assertThat(result).usingRecursiveComparison().isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void twoWinners() {
+        NominationsForm nominations1 = new NominationsForm(USER_1, Map.of(CATEGORY_ONE, PERSON_1));
+        NominationsForm nominations2 = new NominationsForm(USER_2, Map.of(CATEGORY_ONE, PERSON_2));
+        when(nominationsService.getNominations()).thenReturn(List.of(nominations1, nominations2));
+
+        List<CategoryAward> result = testSubject.getAwards();
+        final List<CategoryAward> expectedResult = List.of(new CategoryAward(CATEGORY_ONE, String.format("%s, %s", PERSON_2, PERSON_1), List.of(PERSON_2, PERSON_1)));
         assertThat(result).usingRecursiveComparison().isEqualTo(expectedResult);
     }
 }
