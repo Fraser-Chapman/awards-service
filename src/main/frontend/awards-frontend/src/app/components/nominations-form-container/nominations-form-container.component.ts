18a86934cc07995a4e37efe03a6d2844d5136bfc
fraser.chapman <fraser.chapman@autotrader.co.uk>
2020-11-13 23:07:54 +0000

    various renames

---
+++ src/main/frontend/awards-frontend/src/app/components/nominations-form-container/nominations-form-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nominations-form-container-component',
+  templateUrl: './nominations-form-container.component.html',
+  styleUrls: ['./nominations-form-container.component.scss']
+})
+export class NominationsFormContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
