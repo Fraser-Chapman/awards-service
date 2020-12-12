ff6cb724fe6f62675153b12cd45dd9496664b72a
fraser.chapman <fraser.chapman@autotrader.co.uk>
2020-12-12 04:55:09 +0000

    api-gateway: add api-gateway

---
+++ api-gateway/src/main/java/uk/co/tissueinc/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,24 @@
+package uk.co.tissueinc.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("front-end", route -> route.path("/**").uri("http://localhost:4200"))
+				.route("awards-service-api", route -> route.path("/api/**").uri("http://localhost:8080"))
+				.build();
+	}
+
+}
---
+++ api-gateway/src/main/java/uk/co/tissueinc/apigateway/configuration/GatewayConfig.java
@@ -0,0 +1,9 @@
+package uk.co.tissueinc.apigateway.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+public class GatewayConfig {
+
+}
