9329531fb9d9713184c250d869020c9b34d05f7f
fraser.chapman <fraser.chapman@autotrader.co.uk>
2020-12-20 00:59:31 +0000

    api-gateway: remove unused class and package

--- api-gateway/src/main/java/uk/co/tissueinc/apigateway/configuration/GatewayConfig.java
+++
@@ -1,9 +0,0 @@
-package uk.co.tissueinc.apigateway.configuration;
-
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-
-public class GatewayConfig {
-
-}
