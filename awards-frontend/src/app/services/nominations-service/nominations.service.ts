85d47c63bc301f28a48f8b76ddf58371b0ba6f4c
fraser.chapman <fraser.chapman@autotrader.co.uk>
2020-12-20 01:10:17 +0000

    awards-frontend: remove user id from submit nominations request

--- awards-frontend/src/app/services/nominations-service/nominations.service.ts
+++ awards-frontend/src/app/services/nominations-service/nominations.service.ts
@@ -9,10 +9,9 @@ export class NominationsService {
 
   constructor(private http: HttpClient) { }
 
-  // returning observable of undefined stops the issue of second request not sending
   submitNotifications$(payload: Map<string, string>): Observable<any> {
     const mapAsObject = {};
     payload.forEach((nomination: string, category: string)  => { mapAsObject[category] = nomination; });
-    return this.http.post('http://localhost:8080/api/nominations', {userId: 'x', nominations: mapAsObject});
+    return this.http.post('/api/nominations', mapAsObject);
   }
 }
