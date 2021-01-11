f10dfdf3d04172d9cd719205462a4bdfdeeb57b1
fraser.chapman <fraser.chapman@autotrader.co.uk>
2021-01-11 14:28:07 +0000

    awards-service: Fix test

--- awards-service/src/test/java/uk/co/tissueinc/awardsservice/AwardsServiceApplicationTests.java
+++ awards-service/src/test/java/uk/co/tissueinc/awardsservice/AwardsServiceApplicationTests.java
@@ -3,18 +3,11 @@ package uk.co.tissueinc.awardsservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest
 class AwardsServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
-	@Test
-	public void apiShouldFail() {
-		assertThat(true).isFalse();
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
