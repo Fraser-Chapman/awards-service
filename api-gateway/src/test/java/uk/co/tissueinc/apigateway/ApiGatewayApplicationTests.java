61e66dd46b093f0adac05fcb6773e6449fa212b9
fraser.chapman <fraser.chapman@autotrader.co.uk>
2021-01-11 15:45:21 +0000

    api-gateway: Fix test

--- api-gateway/src/test/java/uk/co/tissueinc/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/uk/co/tissueinc/apigateway/ApiGatewayApplicationTests.java
@@ -3,17 +3,11 @@ package uk.co.tissueinc.apigateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
-	@Test
-	public void gatewayShouldFail() {
-		assertThat(true).isFalse();
-	}
 }
