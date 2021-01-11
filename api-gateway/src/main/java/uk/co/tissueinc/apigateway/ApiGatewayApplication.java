1ef72c16887bf990c51baedec4bc17d38ab29ab7
fraser.chapman <fraser.chapman@autotrader.co.uk>
2021-01-11 11:14:09 +0000

    TT: Get app working in Docker - Move routes config int application.yml. - Front-end doesn't quite work behind gateway yet

--- api-gateway/src/main/java/uk/co/tissueinc/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/uk/co/tissueinc/apigateway/ApiGatewayApplication.java
@@ -13,12 +13,4 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
-	@Bean
-	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route("awards-service-api", route -> route.path("/api/**").uri("http://localhost:8080"))
-				.route("front-end", route -> route.path("/**").uri("http://localhost:4200"))
-				.build();
-	}
-
 }
